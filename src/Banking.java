58beabaefdd6965216ef6d8703fc2ecd13e9e5ef
maaz <maazaf10@gmail.com>
2025-11-11 16:46:20 +0530

    refined q12

--- src/Banking.java
+++ src/Banking.java
@@ -1,9 +1,12 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Banking {
 
-    static float balance;
-    long Amount;
+    float balance;
+    long amount;
+    String accountNumber;
+    static int TotalAccounts;
 
     Scanner sc = new Scanner(System.in);
 
@@ -11,16 +14,73 @@ public class Banking {
     {
 
         Banking obj = new Banking();
-        balance = 50000;
-        obj.deposit(6000);
-        obj.withdraw(200000);
+
 
     }
 
-    void deposit(int amount)
+    Banking()
+    {
+
+        Boolean ValidInput = false;
+        while(!ValidInput){
+
+            System.out.println("Please Enter your Account 12 digit Account Number: ");
+            accountNumber = sc.next();
+            try{
+                if(accountNumber.matches("\\d{12}")){
+                    ValidInput = true;
+                    TotalAccounts++;
+                    balance = (int)(Math.random() * (50000 - 10000 + 1)) + 10000;
+
+                    System.out.println("Welcome User!");
+                    getChoice();
+                }
+
+                else throw new InputMismatchException("Invalid Account Number please try again");
+            }catch (InputMismatchException e)
+            {
+                System.out.println(e);
+            }
+        }
+
+
+
+
+    }
+
+    void getChoice()
+    {
+        System.out.println("Please enter the type of Transaction you want to perform: \n" +
+                "Enter 1 for : Deposit \n" +
+                "Enter 2 for : Withdraw \n" +
+        "Enter 3 for: Display balance");
+        int choice = sc.nextInt();
+
+        switch (choice)
+        {
+            case 1:
+                deposit();
+                break;
+
+            case 2:
+                withdraw();
+                break;
+
+            case 3:
+                getBalance();
+                break;
+
+            default:
+                System.out.println("Invalid Input !");
+        }
+    }
+
+    void deposit()
     {
 
 
+        System.out.println("please enter amount to deposit: ");
+        long amount = sc.nextLong();
         try{
             if(amount<0)
             {
@@ -36,8 +96,12 @@ public class Banking {
         }
     }
 
-    void withdraw(int amount)
+    void withdraw()
     {
+
+
+        System.out.println("please enter amount to withdraw: ");
+        long amount = sc.nextLong();
             try{
 
                 if(balance<amount)
