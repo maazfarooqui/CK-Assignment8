aa7aa77a227271a500a5907f9d23c3635fdb6bc2
maaz <maazaf10@gmail.com>
2025-11-11 17:29:47 +0530

    Refined Banking Application program

--- src/Banking.java
+++ src/Banking.java
@@ -10,10 +10,49 @@ public class Banking {
 
     Scanner sc = new Scanner(System.in);
 
+
     public static void main(String args[])
     {
+        Scanner mainSc = new Scanner(System.in);
 
+        boolean addUser = true;
+
+        while(addUser)
+        {
         Banking obj = new Banking();
+
+        Boolean exit = false;
+            while(!exit)
+            {
+                System.out.println("Do you want to see try for another User? Y/N");
+
+                String ans = mainSc.next();
+                switch (ans)
+                {
+                    case "No":
+                    case "no":
+                    case "n":
+                    case "N":
+                        addUser = false;
+                        exit = true;
+                        System.out.println("exiting program! ");
+                        System.out.println("Total users created : " + TotalAccounts);
+                        break;
+
+                    case "Yes":
+                    case "yes":
+                    case "y":
+                    case "Y":
+                        exit = true;
+                        break;
+
+                    default:
+
+                }
+            }
+
+        }
+
 
 
     }
@@ -50,29 +89,38 @@ public class Banking {
 
     void getChoice()
     {
-        System.out.println("Please enter the type of Transaction you want to perform: \n" +
-                "Enter 1 for : Deposit \n" +
-                "Enter 2 for : Withdraw \n" +
-        "Enter 3 for: Display balance");
-        int choice = sc.nextInt();
+        boolean continueOper = true;
+      while(continueOper)
+      {
+          System.out.println("Please enter the type of Transaction you want to perform: \n" +
+                  "Enter 1 for : Deposit \n" +
+                  "Enter 2 for : Withdraw \n" +
+                  "Enter 3 for: Display balance \n"+
+                  "Enter 4: to Exit");
+          int choice = sc.nextInt();
 
-        switch (choice)
-        {
-            case 1:
-                deposit();
-                break;
+          switch (choice)
+          {
+              case 1:
+                  deposit();
+                  break;
 
-            case 2:
-                withdraw();
-                break;
+              case 2:
+                  withdraw();
+                  break;
 
-            case 3:
-                getBalance();
-                break;
+              case 3:
+                  getBalance();
+                  break;
+              case 4:
+                  System.out.println("Thank you for using our service");
+                  continueOper = false;
+                  break;
 
-            default:
-                System.out.println("Invalid Input !");
-        }
+              default:
+                  System.out.println("Invalid Input !");
+          }
+      }
     }
 
     void deposit()
